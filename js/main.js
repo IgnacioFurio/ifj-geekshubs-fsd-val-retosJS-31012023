411570834307cf3a8b9a048da2e98c7ca183ba9e
Ignacio Furió José <bichoifj@gmail.com>
2023-01-31 17:44:54 +0100

    exercises from 1 to 5 done

---
+++ js/main.js
@@ -0,0 +1,79 @@
+/* 1 -> Diseár un Pseudocódigo de un programa que sume dos números leídos por pantalla y muestre el
+resultado por terminal */
+
+// let num1 = parseInt(prompt("Introduce un número"));
+// let num2 = parseInt(prompt("Introduce otro número"));
+
+// console.log(num1 + num2)
+
+// -----------------------------------------------------------------------------------------------
+
+/* 2 -> Hacer el Pseudocódio de una programa que permita leer 2 números diferentes y nos diga cual 
+es el mayor de los numeros*/
+
+
+// let num1 = parseInt(prompt("Introduce un número"));
+// let num2 = parseInt(prompt("Introduce otro número"));
+
+// if (num1 > num2) {
+//     console.log("El" + " " + num1 + " " + "es mayor que" + " " + num2 )
+// }else if (num1 < num2) {
+//     console.log("El" + " " + num2 + " " + "es mayor que" + " " + num1 )
+// }else {
+//     console.log ("Los números introducidos son el mismo número")
+// }
+
+
+// -----------------------------------------------------------------------------------------------
+
+/* 3 --> Crear un Pseudocódigo de un programa que almacenen 3 números en 3 variables A, B y C. 
+El diagrama debe decidir cúal es  elmayor y cúal es el menor */
+
+// let num1 = parseInt(prompt("Introduce un número"));
+// let num2 = parseInt(prompt("Introduce otro número"));
+// let num3 = parseInt(prompt("Introduce otro número"));
+
+// let pmayor = 0;
+// let pmenor = 0;
+
+// if (num1 < num2) {
+//     pmayor = num2
+//     pmenor = num1
+// }else {
+//     pmayor = num1
+//     pmenor = num2
+// }
+
+// if (pmayor == pmenor == num3){
+//     console.log("Los números introducidos son el mismo número")
+// }else {
+    
+// }
+
+
+// if (pmayor > num3) {1
+//     console.log(pmayor + " " + "es el mayor")    
+// }else {
+//     console.log(num3 + " " + "es el mayor")
+// }
+
+
+// if (pmenor < num3) {
+//     console.log(pmenor + " " + "es el menor")
+// }else {
+//     console.log(num3 + " " + "es el menor")
+// }
+
+// -----------------------------------------------------------------------------------------------
+
+/* 4 --> Realizar el Pseudocódigode un programa que nos calcule la hipotenusa de un triánglo rectángulo, 
+conocidos sus dos catetos. */
+
+// let catetoUno = parseInt(prompt("Introduce uno de los catetos en valor umérico"))
+// let catetoDos = parseInt(prompt("Introduce el otro careto en valor umérico"))
+
+// console.log("la hipotenusa es " + Math.sqrt((catetoUno**2)+(catetoDos**2)))
+
+// -----------------------------------------------------------------------------------------------
+
+/* 5 --> Realiza un Pseudocódigo de un programa que sume 10 números leídos por teclado*/
